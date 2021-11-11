9acea5aabcf71d7773168d49feee6069a49cffae
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2021-11-11 17:05:56 +0300

    constants

---
+++ 08_constants/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	//"fmt"
+	//"math"
+)
+
+func main() {
+
+	/* r := 3.0
+
+	const pi float64 = 3.14
+
+	areaOfCircle := pi * math.Pow(r,2)
+
+	fmt.Println(areaOfCircle) */
+	
+/* 	const x int = 2
+	const y float64 = 3.4
+	const z string = "test"
+	const t bool = false
+
+	fmt.Printf("%T, %v \n",x,x)
+	fmt.Printf("%T, %v \n",y,y)
+	fmt.Printf("%T, %v \n",z,z)
+	fmt.Printf("%T, %v \n",t,t) */
+
+	// constantlara değer ataması yalnızca bir kere yapılır !
+	
+	// const --> compile time
+	// var   --> run time
+
+/* 	y := 3
+
+	const x int = y // y run time elemanı ancak x bir compile time elemanı o yüzden kabul edilemez . 
+
+	fmt.Printf("%T %v \n",y,y)
+	fmt.Printf("%T %v \n",x,x) */
+	
+
+
+}
