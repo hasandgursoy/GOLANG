0204bf14bcde6764dc7778780719eb1ab9648153
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2021-11-11 18:16:01 +0300

    control comparison

---
+++ 10_control_comparison/main.go
@@ -0,0 +1,12 @@
+package main
+
+// Comparison operators
+
+// == Equal != Not Equal
+// < Less than > Greater than
+// <= Less than or equal >= Greater than or equal
+
+// Logical Operators
+// && conditional AND p && q is "if p then q else false"
+// || conditional OR  p || q is "if p then true else q"
+// !  NOT			 !p  is  "not p "
